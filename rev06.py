d425a7a16e250e0540190b3f4c6be08e566687c1
Andrew Johnson <ajohnson15w@gmail.com>
2022-01-05 16:52:41 -0500

    Added up to rev07.py

---
+++ rev06.py
@@ -0,0 +1,29 @@
+#WORKING WITH NUMBERS
+from math import *
+
+print(4)
+print(2.2)
+print(-5.3)
+print(3 + 4.5)
+print(3 * 4 + 5)
+print(3 * (4 + 5))
+print(10 % 3)
+
+my_num = -5
+print(my_num)
+#Cast as a string
+print(str(my_num) + " my favorite number")
+
+#FUNCTIONS
+
+print(abs(my_num))
+print(pow(3,5))
+print(max(4,6))
+print(min(4,6))
+print(round(4.6))
+#round down
+print(floor(3.7))
+#round up
+print(ceil(3.2))
+
+print(sqrt(36))
