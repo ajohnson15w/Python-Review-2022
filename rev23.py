6658e24667f80dec1f0a31b335988f126411f146
Andrew Johnson <ajohnson15w@gmail.com>
2022-01-08 06:23:42 -0500

    Added up to rev23.py

---
+++ rev23.py
@@ -0,0 +1,14 @@
+#BUILD A TRANSLATOR
+def translate(phrase):
+    translation = ""
+    for letter in phrase:
+        if letter in "AEIOUaeiou":
+            if letter.isupper():
+                translation += "G"
+            else:
+                translation += "g"
+        else:
+            translation += letter
+    return translation
+
+print(translate(input("Enter a phrase: ")))
